e1c606aa03fdbc3529c7893e52dd1f04b48d60d4
Rauli <rauli.a.makinen@tuni.fi>
2025-04-14 12:42:10 +0300

    small fixes to person.js

--- osa3/phonebook_backend 3.1/models/person.js
+++ osa3/phonebook_backend 3.1/models/person.js
@@ -27,7 +27,7 @@ personSchema.set('toJSON',{
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
-    delete returnedobject.__v
+    delete returnedObject.__v
   }
 }) 
 
